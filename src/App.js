9d1a75ba7459088901301ddbf9213d288a350b3e
David Grieser <david.grieser@galvanize.com>
2023-02-09 10:54:56 -0600

    working registration form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Login from './Login';
+import Registration from './Registration';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       </header>
       <section>
         <Login></Login>
+        <Registration></Registration>
       </section>
     </div>
   );
