49bf107672a11bf860e15b7fa9e9d801308f666a
David Grieser <david.grieser@galvanize.com>
2023-02-15 16:07:44 -0600

    reduce roles to simple link list

--- src/DisplayRoles.js
+++ src/DisplayRoles.js
@@ -4,6 +4,7 @@ import RoleList from "./RoleList";
 import RoleListContext from "./contexts/RoleListContext";
 import { apiRequestWithToken } from "./IdentityLib";
 import AddRole from "./AddRole";
+import { Link } from "react-router-dom";
 
 const initialRoles = [];
 
@@ -30,7 +31,13 @@ const DisplayRoles = () => {
         <div className="DisplayRoles">
             <AddRole></AddRole>
             <h1>Display Roles</h1>
-            <RoleList roles={roleListState}></RoleList>
+            <ul>
+                {roleListState.map(role => {
+                    return (
+                        <li><Link to={`/displayRoles/${role.name}`}>{role.name}</Link></li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
