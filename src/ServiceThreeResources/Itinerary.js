d2d3c158dbd5d8fe2bb04b458ae866344ad7636e
austin.walker <austin.walker.wgwx@statefarm.com>
2023-05-17 16:33:25 -0500

    update paths for activity & itinerary

---
+++ src/ServiceThreeResources/Itinerary.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Activity from "./Activity";
+
+function Itinerary() {
+    return (
+        <div className="Itinerary">
+
+        </div>
+    )
+}
+
+export default Itinerary; 
