4366b24ef92d4e7eb3e279e6b12a755295e8b98f
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-05-17 12:07:02 -0500

    OrganizerControl displaying at bottom of page

--- src/ServiceOneResources/OrganizerControl.js
+++ src/ServiceOneResources/OrganizerControl.js
@@ -5,8 +5,10 @@ function OrganizerControl() {
   return (
     <div>
       <h2>Organizer Controls</h2>
-      <input type="checkbox" id="public" name="public" value="isPublic"></input><label for="public">Public Event</label>
-      <h3>Show Participants</h3>
+      <input type="checkbox" id="public" name="public" value="isPublic"></input>
+      <label for="public">Public Event</label>
+      <input type="checkbox" id="participants" name="participants" value="participants"></input>
+      <label for="participants">Show Participants</label>
       <button>Edit Event</button>
       <button>Cancel Event</button>
     </div>
