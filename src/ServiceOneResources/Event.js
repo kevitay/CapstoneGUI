eb327cf609e2979c3e3fa300794ee21c79ff491b
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-05-18 16:30:50 -0500

    Parse startDatetime, need to parse endDateTime

--- src/ServiceOneResources/Event.js
+++ src/ServiceOneResources/Event.js
@@ -1,72 +1,113 @@
 import React from "react";
 import EventList from './EventList';
 import OrganizerControl from './OrganizerControl';
+import { useState, useEffect } from 'react';
+
+
+
+
 
 //react event
 export default function Event() {
-    const eventJson = {
-        "creatorId": "aabbcc1234",
-        "organization": "Phils Buds",
-        "name": "St. Patricks Bar Crawl '01",
-        "type": "Social",
-        "description": "Phil's 21st Birthday Pub Crawl",
-        "startDateTime": "2001-01-01T16:00-04:00",
-        "endDateTime": "2001-0102T02:00-04:00",
-        "startLocation": {
-             "startName": "Phil's Tiki Bar",
-             "Address": "123 Example St",
-             "City": "Normal",
-             "State": "IL",
-             "ZipCode": 61761
-        },
-        "endLocation": {
-             "endName": "Greg's Oldtowne Tavern",
-             "Address": "123 Example St",
-             "City": "Normal",
-             "State": "IL",
-             "ZipCode": 61761
-        },
-        "participantListId": "1",
-        "base_cost": "50",
-        "total_cost": "50",
-        "status": "planned",
-        "isPublic": false	
+
+  // const [event, setEvent] = useState({});
+  const [formattedDate, setFormattedDate] = useState('');
+  
+  useEffect(() => {
+    const startDateTime = event.startDateTime;
+    const date = new Date(startDateTime);
+    // Extracting date components
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const month = date.getMonth() + 1; // Months are zero-based, so adding 1
+    const day = date.getDate();
+    const year = date.getFullYear();
+    // Converting to 12-hour format
+    let amPm = 'AM';
+    if (hours >= 12) {
+      amPm = 'PM';
+      if (hours > 12) {
+        hours -= 12;
+      }
     }
 
-    //need to parse dates and times
-    // decide to display locations {possibly add google map integration}
+    const formattedDate = `${month.toString().padStart(2, '0')}/${day.toString().padStart(2, '0')}/${year} ${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')} ${amPm}`;
 
-    return (
-      <div>
-        {/* <h1>Hello World, Im the event component</h1> */}
-        <div className="eventDetails">
-          <h1>{eventJson.name}</h1>
-          <h3>
-            {eventJson.organization} | {eventJson.type}
-          </h3>
-          <p>{eventJson.description}</p>
-        </div>
-        <div className="locationDetails">
-          <h2>When and Where</h2>
-          <h3>Start Time: {eventJson.startDateTime}</h3>
-          <h3>End Time: {eventJson.endDateTime}</h3>
-          <h3>Start Location: {eventJson.startLocation.startName}</h3>
-          <h3>End Location: {eventJson.endLocation.endName}</h3>
-        </div>
-        <div className="baseCost">
-          <h3>Base Cost: ${eventJson.base_cost}</h3>
-        </div>
-        {/* other components go here */}
-        <div className="totalCost">
-          <h3>Total Cost: ${eventJson.total_cost}</h3>
-        </div>
-        <div>
-          <OrganizerControl />
-        </div>
-        <div>
-          <EventList />
-        </div>
+    setFormattedDate(formattedDate);
+    }, []);
+
+  // function getEventById() {
+  //      fetch('/api/events/{id}')
+  //           .then((response) => response.json())
+  //           .then((response) => {
+  //                setEvent(response);
+  //           })
+  //     .catch((err) => console.error(err));
+  // }
+
+
+  const event = {
+    creatorId: 'aabbcc1234',
+    organization: 'Phils Buds',
+    name: "St. Patricks Bar Crawl '01",
+    type: 'Social',
+    description: "Phil's 21st Birthday Pub Crawl",
+    startDateTime: '2001-01-01T16:00-04:00',
+    endDateTime: '2001-01-02T02:00-04:00',
+    startLocation: {
+      startName: "Phil's Tiki Bar",
+      Address: '123 Example St',
+      City: 'Normal',
+      State: 'IL',
+      ZipCode: 61761,
+    },
+    endLocation: {
+      endName: "Greg's Oldtowne Tavern",
+      Address: '123 Example St',
+      City: 'Normal',
+      State: 'IL',
+      ZipCode: 61761,
+    },
+    participantListId: '1',
+    base_cost: '50',
+    total_cost: '50',
+    status: 'planned',
+    isPublic: false,
+  };
+
+  //need to parse dates and times
+  // decide to display locations {possibly add google map integration}
+
+  return (
+    <div>
+      <div className="eventDetails">
+        <h1>{event.name}</h1>
+        <h3>
+          {event.organization} | {event.type}
+        </h3>
+        <p>{event.description}</p>
       </div>
-    );
+      <div className="locationDetails">
+        <h2>When and Where</h2>
+        <h3>Start Time: {formattedDate}</h3>
+        <h3>End Time: {event.endDateTime}</h3>
+        <h3>Start Location: {event.startLocation.startName}</h3>
+        <h3>End Location: {event.endLocation.endName}</h3>
+      </div>
+      <div className="baseCost">
+        <h3>Base Cost: ${event.base_cost}</h3>
+      </div>
+      {/* other components go here */}
+      <div className="totalCost">
+        <h3>Total Cost: ${event.total_cost}</h3>
+      </div>
+      <div>
+        <OrganizerControl />
+      </div>
+      <div>
+        <EventList />
+      </div>
+    </div>
+  );
 }
 
