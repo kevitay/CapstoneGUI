edad5cba417acc54cecec96ea5db6a8930a45221
austin.walker <austin.walker.wgwx@statefarm.com>
2023-05-22 13:37:55 -0500

    remove redundant code

--- src/ServiceThreeResources/ActivityList.js
+++ src/ServiceThreeResources/ActivityList.js
@@ -16,15 +16,13 @@ export default function ActivityList({activityList}) {
         }
     };
 
-    const sortedDateArray = dateArray.sort(); 
-    // console.log("sorted array", sortedDateArray); 
+    dateArray.sort(); 
     // console.log(dateObject); 
 
     return (
         <div>
             <ul>
                 {dateArray.map(date => <div><h2>{date}</h2>{dateObject[date].map((item, index) => <ActivitySummary key={index} activity ={item}/>)}</div>)}
-                {/* { Object.entries(dateObject).map(([key, value]) => <div><h2>{key}</h2>{value.map((item, index) => <ActivitySummary key={index} activity ={item}/> )}</div>) } */}
             </ul>
         </div>
     ); 
