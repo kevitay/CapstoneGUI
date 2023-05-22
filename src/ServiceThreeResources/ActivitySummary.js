5ccdbac900e51a1c102ff2fe070350fb7075d3b7
austin.walker <austin.walker.wgwx@statefarm.com>
2023-05-22 13:58:01 -0500

    remove endTime from ActivitySummary

--- src/ServiceThreeResources/ActivitySummary.js
+++ src/ServiceThreeResources/ActivitySummary.js
@@ -7,7 +7,7 @@ export default function ActivitySummary({activity}) {
         <div>
             <h3>{activity.ActivityName}</h3>
             <p>{activity.Description}</p>
-            <p>Activity Start Time:{activity.StartTime} - Activity End Time: {activity.EndTime}</p>
+            <p>Activity Start Time:{activity.StartTime}</p>
         </div>
     )
 }; 
