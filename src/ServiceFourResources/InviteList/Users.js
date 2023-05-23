7007942f10628fda31404096ffbddd7460dcdee5
Cody Vasey <cody.vasey.kros@statefarm.com>
2023-05-23 14:36:30 -0500

    Invite list functionality

---
+++ src/ServiceFourResources/InviteList/Users.js
@@ -0,0 +1,54 @@
+import React,  {useEffect,  useReducer, useState, useContext }  from "react";
+import './InviteList.css';
+import UserData from "./UserData"; 
+
+
+
+
+// http://a53e50bf576c64141b52293976658417-1117441751.us-west-2.elb.amazonaws.com/api/users
+
+function Users() {
+
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+      };
+
+      const [userState, setUser] = useState([]);
+      const [loading, setLoadState] = useState(false);
+    
+      useEffect(() => {
+        getUsers();
+      },[]);
+
+      function getUsers(){
+        setLoadState(true); 
+        fetch("http://a53e50bf576c64141b52293976658417-1117441751.us-west-2.elb.amazonaws.com/api/users", requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            setUser(result.users)
+        })
+        .then(result => console.log("result log", result))
+        .then(setLoadState(false))
+        .catch(error => console.log('error', error));
+      }
+
+    return (
+    <div className="Users">
+        <form className="invite-form">
+            <table className="user-data-table">
+                <tr>
+                    <th></th>
+                    <th>Name</th>
+                    <th>Location</th>
+                    <th>Invite?</th>
+                </tr>
+                {
+               loading ? "": userState.map((user) => (<UserData invitee={user}></UserData>))
+            } </table>
+            <input type="submit" value="Invite"></input>
+        </form>
+    </div>)
+}
+
+export default Users;
--- src/ServiceFourResources/ServiceFourApp.js
+++ src/ServiceFourResources/ServiceFourApp.js
@@ -1,11 +1,13 @@
 import React from "react";
 import ParticipantsList from "./ParticipantsList/ParticipantsList";
+import InviteList from "./InviteList/InviteList";
 
 function ServiceFourApp() {
     return (
         <div className="ServiceFourApp">
             <h1>Service Four</h1>
             <ParticipantsList></ParticipantsList>
+            <InviteList></InviteList>
         </div>
     )
 }
