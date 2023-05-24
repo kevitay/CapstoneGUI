283def79fdd8ee695f521430821f63663096b943
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-05-24 16:19:33 -0500

    Remove useParams from DeleteEvent

--- src/ServiceOneResources/DeleteEvent.js
+++ src/ServiceOneResources/DeleteEvent.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {useState} from "react";
-import { useParams } from "react-router-dom";
  
 export default function DeleteEvent({id}) {
     let [deleteStatus, setDeleteStatus] = useState("preDelete");
