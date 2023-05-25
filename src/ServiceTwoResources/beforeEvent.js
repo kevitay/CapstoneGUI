292b9b43093b421a5eb91dec145772fccebb45db
Kyle Bounds <kylenbounds@hotmail.com>
2023-05-25 13:15:09 -0500

    updated PUT and post no longer have CORS error

--- src/ServiceTwoResources/beforeEvent.js
+++ src/ServiceTwoResources/beforeEvent.js
@@ -14,9 +14,9 @@ myHeaders.append("Content-Type", "application/json");
 var raw = JSON.stringify({
   "eventId": '3',
   "type": "packing list",
-  "description": "swimsuit",
-  "required": false,
-  "quantity": 1
+  "description": item.item,
+  "required": item.required,
+  "quantity": item.count
 });
 
 var requestOptions = {
@@ -33,68 +33,71 @@ fetch("http://aa2d2637139cf431aa862ecc08beb8fa-796957187.us-west-2.elb.amazonaws
     setPackingList([...packingList, item]);
 };
 
-// var myHeaders = new Headers();
-// myHeaders.append("Content-Type", "application/json");
 
-// var raw = JSON.stringify({
-//   "eventId": "381f2e4c-ba8c-4204-a4fc-c1874fcbc375",
-//   "type": "packing list",
-//   "description": "swimsuit",
-//   "required": false,
-//   "quantity": 1
-// });
 
-// var requestOptions = {
-//   method: 'PUT',
-//   headers: myHeaders,
-//   body: raw,
-//   redirect: 'follow'
-// };
+  const handleUpdateItem = (item) => {
+    // Implement the logic for updating the item at the specified index
+    var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
 
-// fetch("http://aa2d2637139cf431aa862ecc08beb8fa-796957187.us-west-2.elb.amazonaws.com/api/checklist", requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
+var raw = JSON.stringify({
+  "eventId": "3",
+  "type": "packing list",
+  "description": "swimsuit",
+  "required": true,
+  "quantity": 3
+});
 
-//   const handleUpdateItem = (index) => {
-//     // Implement the logic for updating the item at the specified index
-//   };
+var requestOptions = {
+  method: 'PUT',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow',
+  id: 23,
+};
 
-//   const handleDeleteItem = (index) => {
+fetch("http://aa2d2637139cf431aa862ecc08beb8fa-796957187.us-west-2.elb.amazonaws.com/api/checklist/34", requestOptions)
+  .then(response => response.text())
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+  setPackingList([...packingList, item]);
+  };
 
-//     var myHeaders = new Headers();
-// myHeaders.append("Content-Type", "application/json");
+  const handleDeleteItem = (index) => {
 
-// var raw = JSON.stringify({
-//   "eventId": "381f2e4c-ba8c-4204-a4fc-c1874fcbc375",
-//   "type": "packing list",
-//   "description": "swimsuit",
-//   "required": false,
-//   "quantity": 1
-// });
+    var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
 
-// var requestOptions = {
-//   method: 'DELETE',
-//   headers: myHeaders,
-//   body: raw,
-//   redirect: 'follow'
-// };
+var raw = JSON.stringify({
+  "eventId": "381f2e4c-ba8c-4204-a4fc-c1874fcbc375",
+  "type": "packing list",
+  "description": "swimsuit",
+  "required": false,
+  "quantity": 1
+});
 
-// fetch("http://aa2d2637139cf431aa862ecc08beb8fa-796957187.us-west-2.elb.amazonaws.com/api/checklist", requestOptions)
-//   .then(response => response.text())  
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-//     const updatedPackingList = [...packingList];
-//     updatedPackingList.splice(index, 1);
-//     setPackingList(updatedPackingList);
-//   };
+var requestOptions = {
+  method: 'DELETE',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+fetch("http://aa2d2637139cf431aa862ecc08beb8fa-796957187.us-west-2.elb.amazonaws.com/api/checklist/34", requestOptions)
+  .then(response => response.text())  
+  .then(result => console.log(result))
+  .catch(error => console.log('error', error));
+    const updatedPackingList = [...packingList];
+    updatedPackingList.splice(index, 1);
+    setPackingList(updatedPackingList);
+  };
   return (
     <div>
       <AddListItem onAddItem={handleAddItem} />
       <ItemList
         items={packingList}
-        //onUpdateItem={handleUpdateItem}
-        //onDeleteItem={handleDeleteItem}
+        onUpdateItem={handleUpdateItem}
+        onDeleteItem={handleDeleteItem}
       />
     </div>
   );
