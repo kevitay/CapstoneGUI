06768410bee63eae0b29aea9241876ea524d048c
austin.walker <austin.walker.wgwx@statefarm.com>
2023-05-25 14:15:19 -0500

    update hour/minute formatting for activity summary

--- src/ServiceThreeResources/ActivitySummary.js
+++ src/ServiceThreeResources/ActivitySummary.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 export default function ActivitySummary({activity, setDisplayActivityDetails, setCloseActivityDetailsButton}) {
+    const time = new Date(activity.StartTime); 
+    const formattedTime = time.toLocaleTimeString('en-US', {
+        hour: 'numeric',
+        minute: 'numeric'
+    });
+
     return (
         <div>
             <h3 >{activity.ActivityName}</h3>
             <p >{activity.Description}</p>
-            <p >Activity Start Time:{activity.StartTime}</p>
+            <p >Activity Start Time:{formattedTime}</p>
             <button  onClick={() => {setDisplayActivityDetails(activity); setCloseActivityDetailsButton(true)}}>Expand Details</button>
         </div>
     )
