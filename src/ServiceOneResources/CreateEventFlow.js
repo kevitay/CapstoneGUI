9749fe1b1bc03849453a308b68d33f090a62caa7
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-05-30 10:00:30 -0500

    Pass setCreationStep and setEvent to creatEvent

--- src/ServiceOneResources/CreateEventFlow.js
+++ src/ServiceOneResources/CreateEventFlow.js
@@ -5,10 +5,11 @@ import CreateEvent from './CreateEvent';
 function CreateEventFlow() {
   // Steps are: 1-New, 2-Invite, 3-Itinerary, 4-Items, 5-Tasks
   const [creationStep, setCreationStep] = useState(1);
+  const [event, setEvent] = useState(null);
 
   return (
     <div>
-      {creationStep === 1 ? <CreateEvent setCreationStep={setCreationStep} /> : <></>}
+      {creationStep === 1 ? <CreateEvent setCreationStep={setCreationStep} setEvent={setEvent} /> : <></>}
       {creationStep === 2 ? <></> : <></>}
       {creationStep === 3 ? <></> : <></>}
       {creationStep === 4 ? <></> : <></>}
