89c895e222a6d432165e02eb6ada2a59b52488b6
Trenton Wells <TJWELLS82787@gmail.com>
2023-06-02 12:04:34 -0500

    fixing lint error line profileEvent.js 36 authState

--- src/ServiceFourResources/Profile/ProfileEvent.js
+++ src/ServiceFourResources/Profile/ProfileEvent.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect, useContext } from "react";
 import AuthContext from "../../IdentityResources/Contexts/AuthContext.js";
 import UserEvent from "./UserEvent.js";
 
-
 function ProfileEvent() {
 
     const [authState, ] = useContext(AuthContext);
@@ -17,24 +16,25 @@ function ProfileEvent() {
         setLoadState(true);
       
         fetch("http://a53e50bf576c64141b52293976658417-1117441751.us-west-2.elb.amazonaws.com/api/participant/" + authState.username + "/events", requestOptions)
-          .then(response => response.json())
-          .then(result => {
-            const eventPartArr = result.eventParticipants;
-            setUserEventsList(eventPartArr);
-      
-            const fetchEventPromises = eventPartArr.map(eventPart => {
-              return fetch("http://ad0bcd07c990f4a9d9879e71472608fa-1526526031.us-west-2.elb.amazonaws.com/api/event/" + eventPart.eventId)
-                .then(response => response.json());
-            });
-      
-            return Promise.all(fetchEventPromises);
-          })
-          .then(results => {
-            setEventInfo(results);
-            setLoadState(false);
-          })
-          .catch(error => console.log('error', error));
-      }, []);
+        .then(response => response.json())
+        .then(result => {
+          const eventPartArr = result.eventParticipants;
+          setUserEventsList(eventPartArr);
+    
+          const fetchEventPromises = eventPartArr.map(eventPart => {
+            return fetch("http://ad0bcd07c990f4a9d9879e71472608fa-1526526031.us-west-2.elb.amazonaws.com/api/event/" + eventPart.eventId)
+              .then(response => response.json());
+          });
+    
+          return Promise.all(fetchEventPromises);
+        })
+        .then(results => {
+          setEventInfo(results);
+          setLoadState(false);
+        })
+        .catch(error => console.log('error', error));
+    },[authState]);
+
     
     return (
         <>
