d5419bddb1bc78171341a7f17afc07e95df2fc85
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-06-02 15:21:37 -0500

    Merged main into eventComponent

--- src/App.js
+++ src/App.js
@@ -1,80 +1,79 @@
-import React from "react";
-import IdentityApp from "./IdentityResources/IdentityApp";
-import Event from "./ServiceOneResources/Event";
-import ServiceTwoApp from "./ServiceTwoResources/ServiceTwoApp";
-import ServiceThreeApp from "./ServiceThreeResources/ServiceThreeApp";
-import ServiceFourApp from "./ServiceFourResources/ServiceFourApp";
-import { NavLink, Route, Routes } from "react-router-dom";
-import Home from "./Home";
+import React from 'react';
+import IdentityApp from './IdentityResources/IdentityApp';
+import Event from './ServiceOneResources/Event';
+import ServiceTwoApp from './ServiceTwoResources/ServiceTwoApp';
+import ServiceThreeApp from './ServiceThreeResources/ServiceThreeApp';
+import ServiceFourApp from './ServiceFourResources/ServiceFourApp';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import Home from './Home';
 import CreateEventFlow from './ServiceOneResources/CreateEventFlow';
 import { EventProvider } from './ServiceOneResources/EventsContext';
 import EventList from './ServiceOneResources/EventList';
 import EditEvent from './ServiceOneResources/EditEvent';
-import AuthContext from "./IdentityResources/Contexts/AuthContext";
-import { useReducer } from "react";
+import AuthContext from './IdentityResources/Contexts/AuthContext';
+import { useReducer } from 'react';
 
 const authInitialState = {
   username: '',
   token: '',
-}
+};
 
 const authReducer = (state, action) => {
   switch (action.type) {
     case 'saveAuth':
-      const copyOfState = { ...state }
+      const copyOfState = { ...state };
       copyOfState.username = action.payload.username;
       copyOfState.token = action.payload.token;
       return copyOfState;
     default:
       return state;
   }
-}
-
-
+};
 
 function App() {
-  
-  const [authState, authDispatch] = useReducer(authReducer, authInitialState)
+  const [authState, authDispatch] = useReducer(authReducer, authInitialState);
 
-    return (
-      <div className="App">
-        <h1>App</h1>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to={'/'}>Home</NavLink>
-            </li>
-            <li>
-              <NavLink to={'/serviceOne'}>Service One</NavLink>
-            </li>
-            <li>
-              <NavLink to={'/serviceTwo'}>Service Two</NavLink>
-            </li>
-            <li>
-              <NavLink to={'/serviceThree'}>Service Three</NavLink>
-            </li>
-            <li>
-              <NavLink to={'/serviceFour'}>Service Four</NavLink>
-            </li>
-            <li>
-              <NavLink to={'/identity'}>Identity</NavLink>
-            </li>
-          </ul>
-        </nav>
-        <AuthContext.Provider value={[authState, authDispatch]}>
-          <EventProvider>
-            <Routes>
-              <Route path={'/'} element={<Home />}></Route>
-              <Route path={'/serviceOne/event/:id'} element={<Event />}></Route>
-              <Route path={'/serviceOne/createEventFlow'} element={<CreateEventFlow />}></Route>
-              <Route path={'/serviceThree/*'} element={<ServiceThreeApp />}></Route>
-              <Route path={'/serviceFour/*'} element={<ServiceFourApp />}></Route>
-              <Route path={'/identity/*'} element={<IdentityApp />}></Route>
-            </Routes>
-          </EventProvider>
-        </AuthContext.Provider>
-      </div>
-    );
+  return (
+    <div className="App">
+      <h1>App</h1>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to={'/'}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/serviceOne'}>Service One</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/serviceTwo'}>Service Two</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/serviceThree'}>Service Three</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/serviceFour'}>Service Four</NavLink>
+          </li>
+          <li>
+            <NavLink to={'/identity'}>Identity</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <AuthContext.Provider value={[authState, authDispatch]}>
+        <EventProvider>
+          <Routes>
+            <Route path={'/'} element={<Home />}></Route>
+            <Route path={'/serviceOne/eventList'} element={<EventList />}></Route>
+            <Route path={'/serviceOne/event/:id'} element={<Event />}></Route>
+            <Route path={'/serviceOne/editEvent/:id'} element={<EditEvent />}></Route>
+            <Route path={'/serviceOne/createEventFlow'} element={<CreateEventFlow />}></Route>
+            <Route path={'/serviceThree/*'} element={<ServiceThreeApp />}></Route>
+            <Route path={'/serviceFour/*'} element={<ServiceFourApp />}></Route>
+            <Route path={'/identity/*'} element={<IdentityApp />}></Route>
+          </Routes>
+        </EventProvider>
+      </AuthContext.Provider>
+    </div>
+  );
 }
 
 export default App;
