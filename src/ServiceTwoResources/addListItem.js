2569d5b1e5bf08a25fb94a07c696a7ef4a332dba
John Stein <john.h.stein.f2h9@statefarm.com>
2023-06-02 13:15:42 -0500

    REFACTOR: change count to quantity

--- src/ServiceTwoResources/addListItem.js
+++ src/ServiceTwoResources/addListItem.js
@@ -1,12 +1,12 @@
 import React, {useState} from "react";
 
-
-
 function AddListItem({ eventId, setPackingList, packingList }) {
     const [itemInput, setItemInput] = useState('');
     const [required, setRequired] = useState(false);
-    const [count, setCount] = useState(1);
+    const [quantity, setQuantity] = useState(1);
     const [typeInput, setTypeInput] = useState("packing list");
+
+    const checklistUrl = "http://aa2d2637139cf431aa862ecc08beb8fa-796957187.us-west-2.elb.amazonaws.com/api/checklist";
 
     const handleTypeChange = (event) => {
         setTypeInput(event.target.value);
@@ -20,19 +20,19 @@ function AddListItem({ eventId, setPackingList, packingList }) {
         setRequired(!required);
     };
 
-    const handleCountChange=(event) => {
+    const handleQuantityChange=(event) => {
        let value = parseInt(event.target.value);
         if(typeof value === 'number') {
-           setCount(value);}
+           setQuantity(value);}
    }
 
     const addItem = () => {
      if(itemInput !== "") {
-        let newItem = {item: itemInput, type: typeInput, required: required, count: count};
+        let newItem = {item: itemInput, type: typeInput, required: required, quantity: quantity};
         handleAddItem(newItem);
         setRequired(false);
         setItemInput(''); 
-        setCount(1);
+        setQuantity(1);
         setTypeInput("packing list");
         };
     };
@@ -46,7 +46,7 @@ function AddListItem({ eventId, setPackingList, packingList }) {
           "type": item.type,
           "description": item.item,
           "required": item.required,
-          "quantity": item.count
+          "quantity": item.quantity
         });
     
         var requestOptions = {
@@ -56,7 +56,7 @@ function AddListItem({ eventId, setPackingList, packingList }) {
           redirect: 'follow'
         };
     
-        fetch("http://aa2d2637139cf431aa862ecc08beb8fa-796957187.us-west-2.elb.amazonaws.com/api/checklist", requestOptions)
+        fetch(checklistUrl, requestOptions)
           .then(response => response.json())
           .then(result => setPackingList([...packingList, result]))
           .catch(error => console.log('error', error));
@@ -67,7 +67,7 @@ function AddListItem({ eventId, setPackingList, packingList }) {
             <td><input type="text" value={itemInput} onChange={handleInputChange} /></td>
             <td><select name="type" defaultValue="packing list" onChange={handleTypeChange}> <option value="packing list"> Packing List </option>
             <option value="signup list"> Signup List </option> </select></td>
-            <td><input type="number" min="1" value={count} onChange={handleCountChange} /></td> 
+            <td><input type="number" min="1" value={quantity} onChange={handleQuantityChange} /></td> 
             <td><input type="checkbox" checked={required} onChange={handleCheckboxChange} /></td>
             <td><button onClick={addItem}> Add Item </button></td>
         </tr>) 
