e7a80b6d38794156bf04f721cf7ad7dfb3219e5f
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-06-05 12:15:17 -0500

    Format code

--- src/ServiceOneResources/DeleteEvent.js
+++ src/ServiceOneResources/DeleteEvent.js
@@ -16,24 +16,30 @@ export default function DeleteEvent({ id }) {
       .then((response) => console.log('Success- Checklist ' + id + ' Deleted'))
       .catch((error) => console.log(error))
 
-      .then(fetch('http://a53e50bf576c64141b52293976658417-1117441751.us-west-2.elb.amazonaws.com/api/eventParticipants/' + id, {
-        method: 'DELETE',
-      })
-        .then((response) => console.log('Success- Participant List ' + id + ' Deleted'))
-        .catch((error) => console.log(error)))
+      .then(
+        fetch('http://a53e50bf576c64141b52293976658417-1117441751.us-west-2.elb.amazonaws.com/api/eventParticipants/' + id, {
+          method: 'DELETE',
+        })
+          .then((response) => console.log('Success- Participant List ' + id + ' Deleted'))
+          .catch((error) => console.log(error))
+      )
 
-      .then(fetch('http://a53e50bf576c64141b52293976658417-1117441751.us-west-2.elb.amazonaws.com/api/activities/event/' + id, {
-        method: 'DELETE',
-      })
-        .then((response) => console.log('Success- Activity ' + id + ' Deleted'))
-        .catch((error) => console.log(error)))
+      .then(
+        fetch('http://a53e50bf576c64141b52293976658417-1117441751.us-west-2.elb.amazonaws.com/api/activities/event/' + id, {
+          method: 'DELETE',
+        })
+          .then((response) => console.log('Success- Activity ' + id + ' Deleted'))
+          .catch((error) => console.log(error))
+      )
 
-      .then(fetch('http://ad0bcd07c990f4a9d9879e71472608fa-1526526031.us-west-2.elb.amazonaws.com/api/event/' + id, {
-        method: 'DELETE',
-      })
-        .then((response) => console.log('Success- Event ' + id + ' Deleted'))
-        .then((response) => setDeleteStatus('deleted'))
-        .catch((error) => console.log(error)));
+      .then(
+        fetch('http://ad0bcd07c990f4a9d9879e71472608fa-1526526031.us-west-2.elb.amazonaws.com/api/event/' + id, {
+          method: 'DELETE',
+        })
+          .then((response) => console.log('Success- Event ' + id + ' Deleted'))
+          .then((response) => setDeleteStatus('deleted'))
+          .catch((error) => console.log(error))
+      );
   }
 
   return (
