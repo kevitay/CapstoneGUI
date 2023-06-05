8a9c3c8561cce441bf55d8227f4674ed795438ad
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-06-05 12:13:57 -0500

    Update and uncomment fetch calls for delete event

--- src/ServiceOneResources/DeleteEvent.js
+++ src/ServiceOneResources/DeleteEvent.js
@@ -10,33 +10,30 @@ export default function DeleteEvent({ id }) {
   function handleDeleteEvent() {
     setDeleteStatus('pending');
 
-    // TODO: Work with BeforeEvent Team for deleting checklists by eventID
-    //   fetch('http://ad0bcd07c990f4a9d9879e71472608fa-1526526031.us-west-2.elb.amazonaws.com/api/checklist/{eventId}', {
-    //   method: 'DELETE',
-    // })
-    //   .then((response) => console.log('Success- Checklist ' + id + ' Deleted'))
-    //   .catch((error) => console.log(error))
-
-    // TODO: Work with Users Team for deleting participant list by eventID
-    //   .then(fetch('http://a53e50bf576c64141b52293976658417-1117441751.us-west-2.elb.amazonaws.com/api/participants?eventId={eventId}' + id, {
-    //   method: 'DELETE',
-    // })
-    //   .then((response) => console.log('Success- Participant List ' + id + ' Deleted'))
-    //   .catch((error) => console.log(error)));
-
-    // TODO: Work with Itinerary Team for deleting all activities by eventID
-    //   .then(fetch('http://a53e50bf576c64141b52293976658417-1117441751.us-west-2.elb.amazonaws.com/api/activities/{eventId}' + id, {
-    //   method: 'DELETE',
-    // })
-    //   .then((response) => console.log('Success- Activity ' + id + ' Deleted'))
-    //   .catch((error) => console.log(error)));
-
-    fetch('http://ad0bcd07c990f4a9d9879e71472608fa-1526526031.us-west-2.elb.amazonaws.com/api/event/' + id, {
+    fetch('http://ad0bcd07c990f4a9d9879e71472608fa-1526526031.us-west-2.elb.amazonaws.com/api/checklist?eventId=' + id, {
       method: 'DELETE',
     })
-      .then((response) => console.log('Success- Event ' + id + ' Deleted'))
-      .then((response) => setDeleteStatus('deleted'))
-      .catch((error) => console.log(error));
+      .then((response) => console.log('Success- Checklist ' + id + ' Deleted'))
+      .catch((error) => console.log(error))
+
+      .then(fetch('http://a53e50bf576c64141b52293976658417-1117441751.us-west-2.elb.amazonaws.com/api/eventParticipants/' + id, {
+        method: 'DELETE',
+      })
+        .then((response) => console.log('Success- Participant List ' + id + ' Deleted'))
+        .catch((error) => console.log(error)))
+
+      .then(fetch('http://a53e50bf576c64141b52293976658417-1117441751.us-west-2.elb.amazonaws.com/api/activities/event/' + id, {
+        method: 'DELETE',
+      })
+        .then((response) => console.log('Success- Activity ' + id + ' Deleted'))
+        .catch((error) => console.log(error)))
+
+      .then(fetch('http://ad0bcd07c990f4a9d9879e71472608fa-1526526031.us-west-2.elb.amazonaws.com/api/event/' + id, {
+        method: 'DELETE',
+      })
+        .then((response) => console.log('Success- Event ' + id + ' Deleted'))
+        .then((response) => setDeleteStatus('deleted'))
+        .catch((error) => console.log(error)));
   }
 
   return (
