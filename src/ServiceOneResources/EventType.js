36b029738bb0b328c4f9e31e4e735cc26f749740
DevPhilC <philip.christman.jk3f@statefarm.com>
2023-06-06 11:47:27 -0500

    added state to track option selection and other description separately

--- src/ServiceOneResources/EventType.js
+++ src/ServiceOneResources/EventType.js
@@ -4,36 +4,33 @@ import { useState, useEffect } from 'react';
 const optionList = ['--Select event type--', 'Party', 'Celebration', 'Social', 'Reunion', 'Convention', 'Business Meeting', 'Trip', 'Alumni Gathering', 'Club Outing', 'Other'];
 
 function EventType({ eventType, setEventType }) {
+  const [optionListSelection, setOptionListSelection] = useState('--Select event type--');
   const [otherType, setOtherType] = useState('');
 
   useEffect(() => {
     setEventType('Other - ' + otherType);
   }, [otherType, setEventType]);
 
+  useEffect(() => {
+    setEventType(optionListSelection);
+  }, [optionListSelection, setEventType]);
+
   return (
     <>
       <label htmlFor="event-type">Event Type</label>
-      <select name="event-type" id="event-type" value={eventType} onChange={(e) => setEventType(e.target.value)} required>
+      <select name="event-type" id="event-type" value={optionListSelection} onChange={(e) => setOptionListSelection(e.target.value)} required>
         {optionList.map((optionType) => {
           return (
             <>
-              {optionType.startsWith('Other') ? (
-                <option key={optionType} value={optionType}>
-                  Other
-                </option>
-              ) : (
                 <option key={optionType} value={optionType}>
                   {optionType}
                 </option>
-              )
-              }
-
             </>
           );
         })}
       </select>
 
-      {eventType.startsWith('Other') ? (
+      {optionListSelection === 'Other' ? (
         <>
           <label>Other Event Type</label>
           <input type="text" name="eventType" value={otherType} onChange={(e) => setOtherType(e.target.value)} required />
