a1d38367c85bafa21b2d151e23043c199a9b0d2b
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-06-06 09:52:05 -0500

    Build EventType dropdown and set state for event type

---
+++ src/ServiceOneResources/EventType.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const optionList = ['--Select event type--', 'Party', 'Celebration', 'Social', 'Reunion', 'Convention', 'Business Meeting', 'Trip', 'Alumni Gathering', 'Club Outing', 'Other'];
+
+function EventType({eventType, setEventType}) {
+  return (
+    <>
+      <label htmlfor="event-type">Event Type</label>
+      <select name="event-type" id="event-type" value={eventType} onChange={(e) => setEventType(e.target.value)} required>
+        {optionList.map((optionType) => {
+          return <option value={optionType}>{optionType}</option>;
+        })}
+      </select>
+      {/* TODO: If "Other" add terniary and capture user input */}
+    </>
+  );
+}
+
+export default EventType;
