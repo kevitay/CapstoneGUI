7f4efceff1735e9bf3ba259d1ad115578c1a3700
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-06-08 11:14:03 -0500

    Added missing useEffect dependencies

--- src/ServiceOneResources/Event.js
+++ src/ServiceOneResources/Event.js
@@ -42,7 +42,7 @@ export default function Event() {
         setUserIsOwner(true);
       }
     }
-  }, [userIsOwner, authState, currentEvent]);
+  }, [userIsOwner, authState, authState.token, currentEvent]);
 
   function dateFormatter(dateTime) {
     if (dateTime !== null) {
