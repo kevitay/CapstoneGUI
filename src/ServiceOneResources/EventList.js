b00c142d1711f39013cf2fb3d8fe624f43e9593b
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-06-08 10:52:37 -0500

    Final commit for fetch fixes

--- src/ServiceOneResources/EventList.js
+++ src/ServiceOneResources/EventList.js
@@ -3,11 +3,13 @@ import { useEffect, useContext } from 'react';
 import EventBrief from './EventBrief';
 import { EventContext } from './EventsContext';
 import { Stack } from '@mui/material';
+import AuthContext from '../IdentityResources/Contexts/AuthContext';
 
 // const eventsJson = require("./events.json")
 //This component is to display our EventList, inside the return is an EventBrief that organizes the data from the fetch call to display only a brief summary.
 export default function EventList() {
   const { state, dispatch } = useContext(EventContext);
+  const [authState] = useContext(AuthContext);
 
   useEffect(() => {
     function getEvents() {
