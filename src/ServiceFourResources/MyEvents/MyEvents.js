8de8edbf77e09bd1d8cdb3019678ba2118987636
Erick San Juan <esanjuanalvarez@outlook.com>
2023-06-08 23:42:05 -0500

    Red: unable to display img in myevents

--- src/ServiceFourResources/MyEvents/MyEvents.js
+++ src/ServiceFourResources/MyEvents/MyEvents.js
@@ -1,11 +1,23 @@
 import React from "react";
 import CurrentEvents from "./CurrentEvents";
 import CreatorEvents from "../../ServiceOneResources/CreatorEvents";
-import { Divider, Stack } from "@mui/material";
+import { Box, Divider, Stack } from "@mui/material";
 import { Container } from "@mui/system";
 
 function MyEvents() {
+    const boxStyle = {
+        background: "url(MapGirl.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "bottom",
+        bgcolor: "#cfe8fc",
+        height: "50vh",
+        /* Add other desired background properties here */
+      };
+      
     return (
+        <>
+        <Box sx={boxStyle} />
+           <Box/>
         <Container maxWidth='lg'>
         <Stack spacing={5}>
         <CreatorEvents></CreatorEvents>
@@ -13,6 +25,7 @@ function MyEvents() {
         <CurrentEvents></CurrentEvents>
         </Stack>
         </Container>
+        </>
     )
 }
 
