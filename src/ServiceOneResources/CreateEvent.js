2e9041de7e0d76a0c744f4d3bd9da4067b1ce4f2
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-06-09 10:30:35 -0500

    Style createEvent form

--- src/ServiceOneResources/CreateEvent.js
+++ src/ServiceOneResources/CreateEvent.js
@@ -4,6 +4,10 @@ import AuthContext from '../IdentityResources/Contexts/AuthContext';
 //import { EventContext } from "./EventsContext";
 import Login from '../IdentityResources/Login';
 import EventType from './EventType';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
 function CreateEvent({ setCreationStep, setEvent }) {
   const [eventName, setName] = useState('');
@@ -56,60 +60,63 @@ function CreateEvent({ setCreationStep, setEvent }) {
   };
 
   return (
-    <div className="eventSubmit">
-      {!authState.token ? <Login handleClose={handleClose}></Login> : <></>}
-      <h2>Create A New Event</h2>
-      <form
-        action=""
-        method="POST"
-        className="eventForm"
-        onSubmit={(e) => {
-          e.preventDefault();
-          setName('');
-          setOrganization('');
-          setDescription('');
-          setEventType('');
-          setEventCost('');
-          postNewEvent(eventName, organization, description, eventType, eventCost);
-        }}
-      >
-        <label>Event Name</label>
-        <input type="text" name="eventName" value={eventName} onChange={(e) => setName(e.target.value)} required />
-        <br />
-        <br />
-        <label>Organization</label>
-        <input type="text" name="organization" value={organization} onChange={(e) => setOrganization(e.target.value)} required />
-        <br />
-        <br />
-        <EventType eventType={eventType} setEventType={setEventType} />
-        <br />
-        <br />
-        <label>Event Cost</label>
-        <input className="numberField" type="number" min="0.00" name="eventCost" value={eventCost} onChange={(e) => setEventCost(e.target.value)} required />
-        <br />
-        <br />
-        <label>Event Description</label>
-        <br />
-        <textarea name="description" rows="6" cols="33" value={description} onChange={(e) => setDescription(e.target.value)} required />
-        <br />
-        <br />
+    <>
+      <Container maxWidth="md" sx={{ backgroundColor: 'white', height: '100vh' }}>
+        <Paper elevation={5} sx={{ backgroundColor: '#f5f5f5', height: '60vh', alignContent: 'center', padding: '50px', borderRadius: '10px' }}>
+          {!authState.token ? <Login handleClose={handleClose}></Login> : <></>}
+          <form
+            action=""
+            method="POST"
+            className="eventForm"
+            onSubmit={(e) => {
+              e.preventDefault();
+              setName('');
+              setOrganization('');
+              setDescription('');
+              setEventType('');
+              setEventCost('');
+              postNewEvent(eventName, organization, description, eventType, eventCost);
+            }}
+          >
+            <label>Event Name</label>
+            <input type="text" name="eventName" value={eventName} onChange={(e) => setName(e.target.value)} required />
+            <br />
+            <br />
+            <label>Organization</label>
+            <input type="text" name="organization" value={organization} onChange={(e) => setOrganization(e.target.value)} required />
+            <br />
+            <br />
+            <EventType eventType={eventType} setEventType={setEventType} />
+            <br />
+            <br />
+            <label>Event Cost</label>
+            <input className="numberField" type="number" min="0.00" name="eventCost" value={eventCost} onChange={(e) => setEventCost(e.target.value)} required />
+            <br />
+            <br />
+            <label>Event Description</label>
+            <br />
+            <textarea name="description" rows="6" cols="33" value={description} onChange={(e) => setDescription(e.target.value)} required />
+            <br />
+            <br />
 
-        <fieldset>
-          <legend>Public or Private:</legend>
-          <input type="radio" id="public" name="publicPrivate" value={true} checked={isPublic === true} onChange={radioEvent} />
-          <label forhtml="public">Public</label>
-          <br />
-          <input type="radio" id="private" name="publicPrivate" value={false} checked={isPublic === false} onChange={radioEvent} />
-          <label forhtml="private">Private</label>
-          <br />
-        </fieldset>
-        <br />
-        <br />
-        <button disabled={!authState.token} type="submit">
-          Submit
-        </button>
-      </form>
-    </div>
+            <fieldset>
+              <legend>Public or Private:</legend>
+              <input type="radio" id="public" name="publicPrivate" value={true} checked={isPublic === true} onChange={radioEvent} />
+              <label forhtml="public">Public</label>
+              <br />
+              <input type="radio" id="private" name="publicPrivate" value={false} checked={isPublic === false} onChange={radioEvent} />
+              <label forhtml="private">Private</label>
+              <br />
+            </fieldset>
+            <br />
+            <br />
+            <button disabled={!authState.token} type="submit">
+              Submit
+            </button>
+          </form>
+        </Paper>
+      </Container>
+    </>
   );
 }
 
