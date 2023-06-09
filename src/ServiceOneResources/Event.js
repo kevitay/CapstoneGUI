b781d9ef0ae74b987ac26865730a273d5c44536e
DevPhilC <philip.christman.jk3f@statefarm.com>
2023-06-09 12:56:12 -0500

    added mui accordian to the event page

--- src/ServiceOneResources/Event.js
+++ src/ServiceOneResources/Event.js
@@ -5,30 +5,45 @@ import AuthContext from "../IdentityResources/Contexts/AuthContext";
 import { useParams } from "react-router-dom";
 import EditEvent from "./EditEvent";
 import { Container } from "@mui/material";
+import Accordion from "@mui/material/Accordion";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 const emptyLocation = {address:'', city:'',state:'',zipCode:''};
 const initialExtendedFields = {startDateTime:'', endDateTime:'',startLocation: emptyLocation, endLocation:emptyLocation };
+
 
 //react event
 export default function Event() {
   //stores the returned event from event API. After an edit, the event is updated with the result of the post and the extended fields are lost.
   const [currentEvent, setCurrentEvent] = useState(null);
-
+  
   //stores the extended fields that event API sources from itinerary
   const [extendedFields, setExtendedFields] = useState(initialExtendedFields);
   const [authState] = useContext(AuthContext);
-
+  
   //Stores the value whether the logged in user is the owner to allow display of Organizer Control.
   const [userIsOwner, setUserIsOwner] = useState(false);
-
+  
   //Toggle between read only and edit mode. Edit Mode is set from Organizer Control.
   const [editMode, setEditMode] = useState(false);
-
+  
   //used for modified Login component which expects a function from signin button.
   const handleClose = () => {};
-
+  
   let { id } = useParams();
   // console.log(id);
+
+
+
+
+    const [expanded, setExpanded] = useState(false);
+    const handleChange = (panel) => (expanded, isExpanded) => {
+      setExpanded(isExpanded ? panel : false);
+    };
+ 
 
   //loads event details on page load.
   useEffect(() => {
@@ -117,30 +132,102 @@ export default function Event() {
       <>  
         <div className='eventDetails'>
           <h1>{currentEvent.name}</h1>
+        </div>
+        <Accordion
+        sx={{ width: "75%" }}
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+        >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+          >
+          <Typography sx={{ width: "50%", flexShrink: 0 }}>
+            {currentEvent.name} - Event Overview
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        {/* Team Component goes here  */}
           <h3>
             {currentEvent.organization} | {currentEvent.type}
           </h3>
           <p>{currentEvent.description}</p>
           <p>Status: {currentEvent.status}</p>
-        </div>
-        <div className='locationDetails'>
-          <h2>When and Where</h2>
-          <h3>Start Time: {dateFormatter(extendedFields.startDateTime)}</h3>
-          <h3>End Time: {dateFormatter(extendedFields.endDateTime)}</h3>
-          <h3>Start Location: {locationFormatter(extendedFields.startLocation)}</h3>
-          <h3>End Location: {locationFormatter(extendedFields.endLocation)}</h3>
-        </div>
-        <div className='baseCost'>
-          <h3>Base Cost: ${currentEvent.baseCost}</h3>
-        </div>
+          <div className='locationDetails'>
+            <h2>When and Where</h2>
+            <h3>Start Time: {dateFormatter(extendedFields.startDateTime)}</h3>
+            <h3>End Time: {dateFormatter(extendedFields.endDateTime)}</h3>
+            <h3>Start Location: {locationFormatter(extendedFields.startLocation)}</h3>
+            <h3>End Location: {locationFormatter(extendedFields.endLocation)}</h3>
+          </div>
+          <div className='baseCost'>
+            <h3>Base Cost: ${currentEvent.baseCost}</h3>
+          </div>
+        </AccordionDetails>
+      </Accordion>
       </>
       ):(<EditEvent event={currentEvent} setCurrentEvent={setCurrentEvent} setEditMode={setEditMode}/>)}
       <div>
         {(userIsOwner) ? (<OrganizerControl event={ currentEvent } setCurrentEvent={ setCurrentEvent } editMode={editMode} setEditMode={setEditMode} />):(<></>)}
       </div>
-      <p>placeholder for Participants list user view (assume your own component returns empty if not applicable)</p>
-    <p>placeholder for Itinerary user view (assume your own component returns empty if not applicable)</p>    
-    <p>placeholder for Checklist user view (assume your own component returns empty if not applicable)</p>
+      <Accordion
+        sx={{ width: "75%" }}
+        expanded={expanded === "panel2"}
+        onChange={handleChange("panel2")}
+        >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+          >
+          <Typography sx={{ width: "50%", flexShrink: 0 }}>
+            {currentEvent.name} - Event Participants
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        {/* Team Component goes here  */}
+        <p>placeholder for Participants list user view</p>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        sx={{ width: "75%" }}
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+        >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+          >
+          <Typography sx={{ width: "50%", flexShrink: 0 }}>
+            {currentEvent.name} - Event Itinerary
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        {/* Team Component goes here  */}
+        <p>placeholder for Itinerary user view </p>    
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        sx={{ width: "75%" }}
+        expanded={expanded === "panel4"}
+        onChange={handleChange("panel4")}
+        >
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+          >
+          <Typography sx={{ width: "50%", flexShrink: 0 }}>
+            {currentEvent.name} - Event Checklist
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+        {/* Team Component goes here  */}
+        <p>placeholder for Checklist user view</p>
+        </AccordionDetails>
+      </Accordion>
     </Container>
     </>
 
