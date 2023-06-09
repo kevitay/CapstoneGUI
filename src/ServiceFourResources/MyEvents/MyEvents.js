d61dc591670c98fe42e4af18b99c7c1ea2c90bcd
Erick San Juan <esanjuanalvarez@outlook.com>
2023-06-08 23:23:25 -0500

    added creator events and added styling to center on page

--- src/ServiceFourResources/MyEvents/MyEvents.js
+++ src/ServiceFourResources/MyEvents/MyEvents.js
@@ -1,9 +1,18 @@
 import React from "react";
 import CurrentEvents from "./CurrentEvents";
+import CreatorEvents from "../../ServiceOneResources/CreatorEvents";
+import { Divider, Stack } from "@mui/material";
+import { Container } from "@mui/system";
 
 function MyEvents() {
     return (
+        <Container maxWidth='lg'>
+        <Stack spacing={5}>
+        <CreatorEvents></CreatorEvents>
+        <Divider/>
         <CurrentEvents></CurrentEvents>
+        </Stack>
+        </Container>
     )
 }
 
