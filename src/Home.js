a331f20f479716ec82fa572074882941648232a7
Erick San Juan <esanjuanalvarez@outlook.com>
2023-06-08 21:54:57 -0500

    added image to the home page

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,23 @@
+import { Box, Divider, Typography } from "@mui/material";
 import React from "react";
+import EventList from "./ServiceOneResources/EventList";
 
 function Home() {
+    const boxStyle = {
+        background: "url(SailBoatM.jpg)",
+        backgroundSize: "cover",
+        backgroundPosition: "bottom",
+        bgcolor: "#cfe8fc",
+        height: "50vh",
+        /* Add other desired background properties here */
+      };
     return (
         <div className="Home">
-            <h1>Home</h1>
+           <Box sx={boxStyle} />
+           <Box/>
+           <Typography variant="h3" color='inherit'sx={{padding:2, margin:1}}>Public Events</Typography>
+           <Divider/>
+           <EventList/>
         </div>
     )
 }
