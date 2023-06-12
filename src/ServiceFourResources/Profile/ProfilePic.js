a80e9bf308ecfab4cd48eb2484f342fd6931fbcb
Keerthana Ramesh <keerthana.ramesh.xc6r@statefarm.com>
2023-06-12 14:50:15 -0500

    default profile pic fix

--- src/ServiceFourResources/Profile/ProfilePic.js
+++ src/ServiceFourResources/Profile/ProfilePic.js
@@ -20,10 +20,14 @@ const ProfilePic = () => {
         fetchProfilePicture(authState.username);
     }, [authState.username]);
 
+    const imgString = () => {
+        return img ? "data:image/jpg;base64," + img : "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png";
+    }
+
     return (
         <div >
             <Box>
-                <img src={"data:image/jpg;base64," + img} height="200px" width="200px" alt="profile pic" />
+                <img src={imgString()} height="200px" width="200px" alt="profile pic" />
                 <br></br>
                 <ProfilePicUpload fetchProfilePicture={fetchProfilePicture}></ProfilePicUpload>
             </Box>
