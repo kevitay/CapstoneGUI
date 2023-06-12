c2ae1992df21eb66c0c3fb94d88b4cd92defa6f5
Keerthana Ramesh <keerthana.ramesh.xc6r@statefarm.com>
2023-06-12 16:41:08 -0500

    fix unused imports

--- src/ServiceFourResources/Profile/ProfilePage.js
+++ src/ServiceFourResources/Profile/ProfilePage.js
@@ -3,16 +3,9 @@ import UserProfile from "./UserProfile";
 import ProfilePic from "./ProfilePic";
 import Notifications from "../../ServiceTwoResources/notifications";
 import './Profile.css';
-import { Typography, CardContent, Card, Box, Grid, Paper } from "@mui/material";
-import { styled } from '@mui/material/styles';
+import { Typography, CardContent, Card, Box, Grid } from "@mui/material";
 
-const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  }));
+
 
 function ProfilePage() {
     return (
