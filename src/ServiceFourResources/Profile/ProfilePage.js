971e43cf92e70fe7eac9404d9953c2574d736041
Keerthana Ramesh <keerthana.ramesh.xc6r@statefarm.com>
2023-06-12 16:32:21 -0500

    integrated notifications component into user profile

--- src/ServiceFourResources/Profile/ProfilePage.js
+++ src/ServiceFourResources/Profile/ProfilePage.js
@@ -1,18 +1,40 @@
 import React from "react"; 
 import UserProfile from "./UserProfile"; 
 import ProfilePic from "./ProfilePic";
+import Notifications from "../../ServiceTwoResources/notifications";
 import './Profile.css';
-import { Typography, CardContent, Card } from "@mui/material";
+import { Typography, CardContent, Card, Box, Grid, Paper } from "@mui/material";
+import { styled } from '@mui/material/styles';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
 
 function ProfilePage() {
     return (
         <div style={{margin: '10%'}}>
-        <Card sx={{ maxWidth: 500 }}>
+        <Card sx={{ maxWidth: 1000 }}>
           <CardContent>
-            <Typography variant="h4">User Profile</Typography>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item lg={4}>
+                      <Typography sx={{ mb: 10 }}>
+                        <ProfilePic></ProfilePic>
+                      </Typography>
+                    </Grid>
+                    <Grid item lg={4}>
+                    <Typography sx={{ mt: 10 }}>
+                        <UserProfile></UserProfile>
+                    </Typography>
+                    </Grid>
+                </Grid>
+            </Box>
             <Typography sx={{ mb: 50 }} color="text.secondary">
-                <ProfilePic></ProfilePic>
-                <UserProfile></UserProfile>
+                <Notifications></Notifications>
             </Typography>
           </CardContent>
         </Card>
