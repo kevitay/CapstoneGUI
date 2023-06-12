b2f00a317603e0fb7b758a8213d3c6693b335a89
Cody Vasey <cody.vasey.kros@statefarm.com>
2023-06-12 16:36:03 -0500

    init commit

--- src/ServiceFourResources/ParticipantsList/NameSearch.js
+++ src/ServiceFourResources/ParticipantsList/NameSearch.js
@@ -1,32 +1,42 @@
 import { Button } from "@mui/material";
-import {React, useState, useEffect} from "react";
+import { React, useState, useEffect } from "react";
 
 function NameSearch({ resetStatus, participantState, setEventParticipants }) {
+  const [searchTerm, setSearchTerm] = useState("");
 
-    const [searchTerm, setSearchTerm] = useState("");
+  const captureCategory = (event) => {
+    event.preventDefault();
+    const newValue = event.target.value;
+    setSearchTerm(newValue);
+  }
 
-    const captureCategory = (event) => {
-        event.preventDefault()
-        const newValue = event.target.value;
-        console.log(newValue)
-        setSearchTerm(newValue);
-    }
+  function handleSearch(e) {
+    e.preventDefault();
+    const categories = participantState.filter(x =>
+      (x.user.firstName + " " + x.user.lastName).toLowerCase().includes(searchTerm.toLowerCase()) ||
+      x.user.firstName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      x.user.lastName.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setEventParticipants(categories);
+  }
 
-    function handleSearch(e) {
-        console.log(searchTerm)
-        e.preventDefault()
-        const categories = participantState.filter(x => x.user.firstName + " " + x.user.lastName === searchTerm);
-        setEventParticipants(categories);
-    }
+  useEffect(() => {
+    setSearchTerm("");
+  }, [resetStatus]);
 
-    useEffect(() => { setSearchTerm("") }, [resetStatus]);
-
-    return (
-        <form onSubmit={(e) => handleSearch(e)}>
-            <input onChange={(e) => captureCategory(e)} type="text" value={searchTerm} placeholder="Enter Name"></input>
-            <Button sx={{ml:'.5em',height:'1.5em'}} variant="contained" type="submit">Search</Button>
-        </form>
-    )
+  return (
+    <form onSubmit={(e) => handleSearch(e)}>
+      <input
+        onChange={(e) => captureCategory(e)}
+        type="text"
+        value={searchTerm}
+        placeholder="Enter Name"
+      />
+      <Button sx={{ ml: '.5em', height: '1.5em' }} variant="contained" type="submit">
+        Search
+      </Button>
+    </form>
+  )
 }
 
 export default NameSearch;
