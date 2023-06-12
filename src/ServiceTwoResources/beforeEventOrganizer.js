0589de9b509f0be82e7cf08779b5e273f652703f
Russhi Gibbs <ladyg21218@gmail.com>
2023-06-12 13:42:22 -0500

    updated the submit button

--- src/ServiceTwoResources/beforeEventOrganizer.js
+++ src/ServiceTwoResources/beforeEventOrganizer.js
@@ -1,16 +1,21 @@
 import React from "react";
 import BeforeEvent from "./beforeEvent";
 import { Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
-function OrganizerViewApp({ eventId }) {
-
+function BeforeEventOrganizer({ eventId, setCreationStep }) {
+    const navigate = useNavigate();
+    function handleEventClick() {
+        setCreationStep(5);
+        navigate(`/serviceOne/event/${eventId}`);
+    }
     return (
         <>
-            <Typography variant="h1">Add Essential Items</Typography>
+            <Typography variant="h4">Add Essential Items</Typography>
             <BeforeEvent eventId={eventId} />
-            <Button sx={{ width: 175 }} variant="contained" onSubmit={() => {  }}> Submit </Button>
+            <Button sx={{ width: 175 }} variant="contained" onClick={() => { handleEventClick() }}> Submit </Button>
         </>
     )
 }
 
-export default OrganizerViewApp;
+export default BeforeEventOrganizer;
