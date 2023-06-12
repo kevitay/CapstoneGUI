355caa519cdda75c563196a84a50469f617fc2c7
jacobopperman <jacob.opperman.vabqjb@statefarm.com>
2023-06-12 10:25:01 -0500

    refactor of EventImageNav

--- src/ServiceFourResources/EventImages/EventImageNav.js
+++ src/ServiceFourResources/EventImages/EventImageNav.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { IconButton } from "@mui/material";
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
 
 function EventImageNav({ eventId }) {
+    const navigate = useNavigate();
+
+    function handlePhotoIconClick() {
+        navigate('/eventImages/' + eventId);
+    }
+
     return (
-        <button><NavLink to={'/eventImages/' + eventId}>Event Images</NavLink></button>
+        //<button><NavLink to={'/eventImages/' + eventId}>Event Images</NavLink></button>
+        <IconButton onClick={handlePhotoIconClick}>
+            <PhotoLibraryIcon />
+        </IconButton>
     )
 }
 
